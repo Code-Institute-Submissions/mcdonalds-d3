562cc57eda85748cc3bd1045cc04c01c5e5d90d7
Ignas Grigaravicius <kickeris@gmail.com>
2017-06-11 13:22:02 +0100

    second commit

--- mcdonalds.py
+++ mcdonalds.py
@@ -28,7 +28,11 @@ def donor_projects():
  
     # A constant that defines the record fields that we wish to retrieve.
     FIELDS = {
-        '_id': False, 'County': True
+        '_id': False,'Category':True, 'Cat':True, 'Calories':True, 
+        'Item':True, 'Total Fat (% Daily Value)':True, 'Saturated Fat (% Daily Value)':True, 'Cholesterol (% Daily Value)':True, 
+        'Sodium (% Daily Value)':True, 'Carbohydrates (% Daily Value)':True, 'Dietary Fiber (% Daily Value)':True, 'Sugars':True, 
+        'Protein':True, 'Vitamin A (% Daily Value)':True, 'Vitamin C (% Daily Value)':True, 'Calcium (% Daily Value)':True, 
+        'Iron (% Daily Value)':True
     }
  
     # Open a connection to MongoDB using a with statement such that the
--- static/js/graph.js
+++ static/js/graph.js
@@ -9,116 +9,113 @@ queue()
    var ndx = crossfilter(projectsJson);
  
    //Define Dimensions
-   var CountyDim = ndx.dimension(function (d) {
-       return d["County"];
+   var CatDim = ndx.dimension(function (d) {
+       return d["Cat"];
+    });
+
+   var ItemDim = ndx.dimension(function (d) {
+       return d["Item"];
    });
+   
+//    var totalSunnyHoursPerYearDim = ndx.dimension(function (d) {
+//        return d[""];
+//    });
 
-   var CodeDim = ndx.dimension(function (d) {
-       return d[""];
-    });
+//    var bulb = ndx.dimension(function (d) {
+//        return d[""];
+//     });
 
-   var avrhoursDim = ndx.dimension(function (d) {
-       return d[""];
-    });
-   var totalSunnyHoursPerYearDim = ndx.dimension(function (d) {
-       return d[""];
-   });
-
-   var bulb = ndx.dimension(function (d) {
-       return d[""];
-    });
-
-    var bulbled = ndx.dimension(function (d) {
-       return d[""];
-    });
+//     var bulbled = ndx.dimension(function (d) {
+//        return d[""];
+//     });
 
 
    //Calculate metrics
 
-   var bulbchart = bulb.group()
-   var bulbchart2 = bulbled.group()
+//    var bulbchart = bulb.group()
+//    var bulbchart2 = bulbled.group()
 
 
-    var numTotalavrhours = CodeDim.group().reduceSum(function (d) {
-       return d[""];});
+    var numCalories = ItemDim.group().reduceSum(function (d) {
+       return d["Calories"];});
 
-    var numTotalSunnyHoursPerYear = CodeDim.group().reduceSum(function (d) {
-       return d[""];});
+    // var numTotalSunnyHoursPerYear = CatDim.group().reduceSum(function (d) {
+    //    return d[""];});
 
-    var teslagroup = CodeDim.group().reduceSum(function (d) {
-       return d[""];});
+    // var teslagroup = CatDim.group().reduceSum(function (d) {
+    //    return d[""];});
 
-    var stateGroup = CountyDim.group()
+    var CatGroup = ItemDim.group()
 
  
    //Charts
-   var a1 = dc.pieChart("#chart name1");
-   var a2 = dc.pieChart("#chart name2");
-   var a3 = dc.barChart("#chart name3");
-   var a4 = dc.barChart("#chart name4");
-   var a5 = dc.lineChart("#chart name5");
+   var FatChart = dc.pieChart("#chart name1");
+//    var a2 = dc.pieChart("#chart name2");
+   var CaloriesChart = dc.barChart("#calorieschart");
+//    var a4 = dc.barChart("#chart name4");
+//    var a5 = dc.lineChart("#chart name5");
 
     
-    a1
-        .height(213)
-       .radius(90)
-       .innerRadius(40)
-       .transitionDuration(1500)
-       .dimension(bulbled)
-       .legend(dc.legend().x(10).y(10).gap(4))
-       .group(bulbchart2);
+    // a1
+    //     .height(213)
+    //    .radius(90)
+    //    .innerRadius(40)
+    //    .transitionDuration(1500)
+    //    .dimension(bulbled)
+    //    .legend(dc.legend().x(10).y(10).gap(4))
+    //    .group(bulbchart2);
 
-    a2
-        .height(213)
-       .radius(90)
-       .innerRadius(40)
-       .transitionDuration(1500)
-       .dimension(bulb)
-       .legend(dc.legend().x(10).y(40).gap(5))
-       .group(bulbchart);
+    // a2
+    //     .height(213)
+    //    .radius(90)
+    //    .innerRadius(40)
+    //    .transitionDuration(1500)
+    //    .dimension(bulb)
+    //    .legend(dc.legend().x(10).y(40).gap(5))
+    //    .group(bulbchart);
 
-    a5
-       .width(900)
-       .height(500)
-       .margins({top: 10, right: 50, bottom: 40, left: 50})
-       .dimension(CodeDim)
-       .group(teslagroup)
-       .transitionDuration(500)
-       .x(d3.scale.ordinal())
-       .y(d3.scale.linear().domain([5000, 8000]))
-       .xUnits(dc.units.ordinal)
-       .renderDataPoints(true)
-       .renderArea(true)
-       .yAxis().ticks(10);
+    // a5
+    //    .width(900)
+    //    .height(500)
+    //    .margins({top: 10, right: 50, bottom: 40, left: 50})
+    //    .dimension(CodeDim)
+    //    .group(teslagroup)
+    //    .transitionDuration(500)
+    //    .x(d3.scale.ordinal())
+    //    .y(d3.scale.linear().domain([5000, 8000]))
+    //    .xUnits(dc.units.ordinal)
+    //    .renderDataPoints(true)
+    //    .renderArea(true)
+    //    .yAxis().ticks(10);
 
-    a3
+    // a4
+    //    .width(1200)
+    //    .height(300)
+    //    .margins({top: 10, right: 50, bottom: 40, left: 50})
+    //    .dimension(CodeDim)
+    //    .group(numTotalSunnyHoursPerYear)
+    //    .transitionDuration(500)
+    //    .x(d3.scale.ordinal())
+    //    .y(d3.scale.linear().domain([500, 900]))
+    //    .xUnits(dc.units.ordinal)
+    //    .yAxis().ticks(10);
+
+    CaloriesChart
        .width(1200)
-       .height(300)
-       .margins({top: 10, right: 50, bottom: 40, left: 50})
-       .dimension(CodeDim)
-       .group(numTotalSunnyHoursPerYear)
+       .height(600)
+       .margins({top: 10, right: 50, bottom: 300, left: 50})
+       .dimension(ItemDim)
+       .group(numCalories)
        .transitionDuration(500)
        .x(d3.scale.ordinal())
-       .y(d3.scale.linear().domain([500, 900]))
-       .xUnits(dc.units.ordinal)
-       .yAxis().ticks(10);
-
-    a4
-       .width(1200)
-       .height(300)
-       .margins({top: 10, right: 50, bottom: 40, left: 50})
-       .dimension(CodeDim)
-       .group(numTotalavrhours)
-       .transitionDuration(500)
-       .x(d3.scale.ordinal())
-       .y(d3.scale.linear().domain([0, 80]))
+       .y(d3.scale.linear().domain([300, 1200]))
        .xUnits(dc.units.ordinal)
        .yAxis().ticks(10);
        
 
     selectField = dc.selectMenu('#menu-select')
-       .dimension(CountyDim)
-       .group(stateGroup);
+       .dimension(ItemDim)
+       .group(numCalories);
 
    dc.renderAll();
 }
